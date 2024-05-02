f9a4e51e47631ca1db97b0b6a2ffd919b07ab15b
dohits <brrspace@gmail.com>
2024-05-02 19:20:29 +0900

    main page post layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main className="flex justify-center">
@@ -19,8 +22,14 @@ export default function Home() {
             <img alt="Hits" src="https://hits.sh/dohits.github.io.svg?view=today-total&style=for-the-badge&label=visitors&color=09090B&labelColor=09090B&logo=github"/>
           </a>
         </li>
-        <li className="mt-10 text-white">
+        <li className="mt-10 w-full h-full text-white">
           recent / popular
+          <div className="mt-10 p-1 rounded-3xl overflow-hidden flex flex-col">
+            <img className="object-cover h-full w-full" src='/posts/공사중.png' alt="postImg"/>
+            <div className="text-sm p-3">This is Title 이것은 제목입니다</div>
+            <div className="text-xs p-2">Detail 내용 내용 내용 입니다.</div>
+            <div className="absolute bg-zinc-600 z-[10]">카테고리 영역</div>
+          </div>
         </li>
       </ul>
     </main>
