016f8a19ef4927c6d11f6208e55fce798a3906ca
dohits <brrspace@gmail.com>
2024-05-03 15:25:42 +0900

    disqus comment

--- src/app/(route)/comment/page.tsx
+++ src/app/(route)/comment/page.tsx
@@ -3,7 +3,7 @@ import DisqusComponent from "@/app/_components/DisqusComponent"
 export default function Comment() {
   return (
     <main className="w-full flex flex-col bg-zinc-600 rounded-3xl mb-4 p-0">
-      <div className="w-full whitespace-nowrap scale-[0.85]">
+      <div className="w-full whitespace-nowrap scale-[0.80]">
         <DisqusComponent identifier="tester" title="테스트"/>
       </div>
     </main>
