ab8fbae61be386c4872be97d32a120e954b65d7e
dohits <brrspace@gmail.com>
2024-05-10 15:20:32 +0900

    post page 코드  리팩토링

--- src/app/(route)/posts/[post_category]/list/(redirect)/page.tsx
+++ src/app/(route)/posts/[post_category]/list/(redirect)/page.tsx
@@ -1,4 +1,26 @@
+import {getPostList} from "@/app/_utils/_lib/postParser";
 import { redirect } from 'next/navigation';
-export default async function allpostRedirectPage() {
-  redirect('/posts/list/1');
+ 
+export default async function Posts({params}:{params:any}) {
+  const {post_category} = params;
+  redirect(`/posts/${post_category}/list/1`);
 }
+
+export async function generateStaticParams() {
+  const gpl =  await getPostList();
+  const result = gpl.map((gpldata) => ({
+    post_category: gpldata.postAbstract.category ,
+    // 로컬환경에서 테스트시 아래 주석 해제, github pages 는 url 자동인코딩되므로 위의 코드 사용
+    //post_category: encodeURI(gpldata.postAbstract.category||'error') ,
+  }));
+  return result;
+}  
+
+/* 
+    경로 생성 후 없는 경로로 진입시 
+    개발환경에서 error 뜨더라도 
+    배포시에는 정상적으로 not-found 페이지 진입됨.
+*/
+
+
+
