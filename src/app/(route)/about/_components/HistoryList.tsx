80e1e75bd03a74019c7837a3583dd2dd23c40221
dohits <brrspace@gmail.com>
2024-05-10 19:18:52 +0900

    add about -> history design

--- src/app/(route)/about/_components/HistoryList.tsx
+++ src/app/(route)/about/_components/HistoryList.tsx
@@ -2,31 +2,55 @@
 export default function HistoryList({
   title,
   contents,
-  date
+  date,
+  unCircle,
+  contourLine,
 }:{
-  title:string,
-  contents:string,
-  date:string,
+  title?:string,
+  contents?:string,
+  date?:string,
+  unCircle?:boolean,
+  contourLine?:string,
 }){
   return(
     <>
-      <div className="bg-zinc-950 w-full rounded-xl flex">
-        <div className="text-white flex w-40 text-white pt-8 justify-center text-sm bg-zinc-800 pb-8 italic">
-          {date}
-        </div>
-        <div className="text-white flex w-full text-white
-        border-l-8 border-emerald-400 border-solid pl-8 pt-6
-        ">
-          <div className="bg-zinc-950 min-w-10 w-10 h-10 relative -left-[55px] rounded-full 
-          border-emerald-400 border-solid border-4
+      {contourLine ? 
+        // contourLine == 값 있을경우
+        <div className="bg-zinc-800 rounded-r-full">
+          <div className="bg-zinc-950 min-w-10 w-full relative rounded-full 
+          border-emerald-400 border-solid border-2
           ">
-          </div>
-          <ul>
-            <li className="text-xl zinc-100 text-wrap italic font-thin">{title}</li>
-            <li className="text-xs text-zinc-400 text-wrap font-thin">{contents}</li>
-          </ul>
+            <div className="text-white flex w-full h-full justify-center items-center mt-4 mb-4 text-2xl italic">{contourLine}</div>
+          </div> 
         </div>
-      </div>
+        :
+        // contourLine == "" || contourLine == undefined
+        <div className="bg-zinc-950 w-full rounded-xl flex">
+          <div className="text-white flex w-40 text-white pt-8 justify-center text-sm bg-zinc-800 pb-8 italic">
+            {date}
+          </div>
+          <div className="text-white flex w-full text-white
+          border-l-4 border-emerald-400 border-solid pl-8 pt-6
+          ">
+            
+            {unCircle ? 
+            // unCircle == true
+            <div className="ml-10">
+            </div>
+            :
+            // unCircle == false || unCircle == undefined
+              <div className="bg-zinc-950 min-w-10 w-10 h-10 relative -left-[55px] rounded-full 
+              border-emerald-400 border-solid border-4
+              ">
+              </div>
+            }
+            <ul>
+              <li className="text-xl zinc-100 text-wrap italic font-thin">{title}</li>
+              <li className="text-xs text-zinc-400 text-wrap font-thin mb-4">{contents}</li>
+            </ul>
+          </div>
+        </div>
+      }
     </>
   )
 }
