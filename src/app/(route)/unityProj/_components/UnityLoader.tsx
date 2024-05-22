72e94fc63d9a28382be7e457debc85409ec49355
dohits <brrspace@gmail.com>
2024-05-22 15:34:54 +0900

    unity test

--- src/app/(route)/unityProj/_components/UnityLoader.tsx
+++ src/app/(route)/unityProj/_components/UnityLoader.tsx
@@ -3,10 +3,10 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 
 export default function UnityLoader() {
   const { unityProvider } = useUnityContext({
-    loaderUrl: "./_buildData/Build/build.loader.js",
-    dataUrl: "./_buildData/Build/build.data",
-    frameworkUrl: "./_buildData/Build/build.framework.js",
-    codeUrl: "./_buildData/Build/build.wasm",
+    loaderUrl: "/_buildData/Build/build.loader.js",
+    dataUrl: "/_buildData/Build/build.data",
+    frameworkUrl: "/_buildData/Build/build.framework.js",
+    codeUrl: "/_buildData/Build/build.wasm",
   });
 
   return (
